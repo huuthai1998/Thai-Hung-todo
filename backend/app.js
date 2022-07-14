b1494a68710b9317492a98cf09f6e8ae4285c6de
hungcndev <nhonhungcao@gmail.com>
2022-07-14 11:35:38 +0700

    add winston logger, remove views and public

--- backend/app.js
+++ backend/app.js
@@ -2,22 +2,17 @@ var createError = require("http-errors");
 var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
-var logger = require("morgan");
+var logger = require("./configs/morganMiddleware");
 
 var todosRouter = require("./routes/todos");
 var usersRouter = require("./routes/users");
 
 var app = express();
 
-// view engine setup
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "jade");
-
-app.use(logger("dev"));
+app.use(logger);
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/todos", todosRouter);
 app.use("/users", usersRouter);
