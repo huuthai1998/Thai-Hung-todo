e4b98c5140567396d269efeef1925b686105100c
Thai <huuthai1998@gmail.com>
2022-07-19 11:27:56 +0700

    added navbar prototype

--- frontend/src/contexts/store.js
+++ frontend/src/contexts/store.js
@@ -1,38 +1,30 @@
 import React, { useReducer } from "react";
 import { ADD_FAVORITE, REMOVE_FAVORITE, SET_TOKEN } from "../constant";
 
-const postReducer = (state, action) => {
+const authReducer = (state, action) => {
   switch (action.type) {
-    case ADD_FAVORITE:
-      return { favorites: [...state.favorites, action.payload.id] };
-    case REMOVE_FAVORITE:
-      return {
-        favorites: state.favorites.filter((id) => id !== action.payload.id),
-      };
     case SET_TOKEN:
       return {
         ...state,
         token: action.payload.token,
       };
-
     default:
       return state;
   }
 };
 
 const initialState = {
-  favorites: [],
   token: "",
 };
-const PostContext = React.createContext({
-  postContext: initialState,
+const AuthContext = React.createContext({
+  authContext: initialState,
   addFavorite: () => {},
   removeFavorite: () => {},
   setToken: () => {},
 });
 
-export const PostProvider = (props) => {
-  const [postContext, dispatch] = useReducer(postReducer, initialState);
+export const AuthProvider = (props) => {
+  const [postContext, dispatch] = useReducer(authReducer, initialState);
 
   const addFavorite = (id) => {
     dispatch({
@@ -56,17 +48,17 @@ export const PostProvider = (props) => {
   };
 
   return (
-    <PostContext.Provider
+    <AuthContext.Provider
       value={{
-        postContext,
+        authContext: postContext,
         addFavorite,
         removeFavorite,
         setToken,
       }}
     >
       {props.children}
-    </PostContext.Provider>
+    </AuthContext.Provider>
   );
 };
 
-export const useTodoContext = () => React.useContext(PostContext);
+export const useTodoContext = () => React.useContext(AuthContext);
